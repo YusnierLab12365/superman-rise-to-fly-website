59974e8c402ac54804221fe92c9c953a756b91cc
Yusnier Ríos <riosyusnier@gmail.com>
2025-11-24 21:55:40 -0600

    Prueba deploy funcional

--- index.php
+++ index.php
@@ -654,7 +654,7 @@ $usuario_id = $_SESSION["usuario_id"] ?? null;
     const itemsImg = trackImg.children;
     const totalItemsImg = itemsImg.length;
 
-    // Clona y añade los elementos de la galería de imágenes porfin funciono esta vaina jvg
+    // Clona y añade los elementos de la galería de imágenes porfin funciono esta vaina jvg DE NUEVO HAREMOS QE JALE  ESTA COSA YA ME QUIERO DORMIR 
     for (let i = 0; i < totalItemsImg; i++) {
         const clone = itemsImg[i].cloneNode(true);
         trackImg.appendChild(clone);
